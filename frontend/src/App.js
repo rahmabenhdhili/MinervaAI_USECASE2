4abb43a5c5d16f6f35066607388167045c18b61c
Islem Labidi <labidiislem971@gmail.com>
2026-01-27 23:18:45 +0100

    feat✨: adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SearchForm from './components/B2BFrom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Dinero - B2B Supplier finder
+      </h1>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
